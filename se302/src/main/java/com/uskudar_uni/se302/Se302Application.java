34daba8e335e014ab27ef6271722821b094b87a0
Selim Günay <selimgnytr@gmail.com>
2026-03-23 03:58:11 +0300

    Initialize

---
+++ se302/src/main/java/com/uskudar_uni/se302/Se302Application.java
@@ -0,0 +1,13 @@
+package com.uskudar_uni.se302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se302Application.class, args);
+	}
+
+}
---
+++ se302/src/test/java/com/uskudar_uni/se302/Se302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uskudar_uni.se302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se302ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
